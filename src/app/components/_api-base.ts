257497be2705e2d6461e0ce5d62970c01d9bbd60
azhar <azhar2kazi@gmail.com>
2025-08-24 09:31:15 +0530

    updated structure

--- src/app/components/_api-base.ts
+++ src/app/components/_api-base.ts
@@ -1,1 +1,3 @@
-export const API_BASE = 'https://stunning-space-palm-tree-wr4r47g9576vf9xj9-8080.app.github.dev';
+//export const API_BASE = 'https://stunning-space-palm-tree-wr4r47g9576vf9xj9-8080.app.github.dev';
+export const API_BASE = 'https://maktab-school.onrender.com';
+
